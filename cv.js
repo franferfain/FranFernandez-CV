ca734acb1a197a9e577d03e762d18349d288cb54
Francisco Fernandez <franferfain@gmail.com>
2021-06-27 22:52:09 -0300

    Upgrade! new functions

--- cv.js
+++ cv.js
@@ -1,9 +1,23 @@
+// Función para que cambie a Darkmode
+//  Darkmode Function
 function darkMode() {
   let element = document.body;
   element.classList.toggle("dark-mode");
 }
 
+// Función para que cambie de idioma
+//  Language change function
 function english() {
   let element = document.body;
   element.classList.toggle("english");
 }
+
+// Función para que aparesca la flecha que lleva al inicio luego de scroll down.
+// Function to make the up arrow visible before scroll down.
+window.onscroll = function () {
+  if (pageYOffset >= 200) {
+    document.getElementById("back-to-top").style.visibility = "visible";
+  } else {
+    document.getElementById("back-to-top").style.visibility = "hidden";
+  }
+};
